424be41432625d65e244f3be21a29f2b10200f09
Hoang Nam <cachiengion314@gmail.com>
2020-11-02 13:44:11 +0700

    update hwork-2

--- Session-4/Homework/Hwork-2/app.js
+++ Session-4/Homework/Hwork-2/app.js
@@ -6,13 +6,15 @@ class CheckedNode {
     }
     has(node) {
         let hasNode = false;
-        if (this.checkedNodes.includes(node)) {
-            hasNode = true;
+        for (let value of this.checkedNodes) {
+            if (node.phoneNumber == value.phoneNumber) {
+                hasNode = true;
+            }
         }
         return hasNode;
     }
     add(node) {
-        if (!this.checkedNodes.includes(node)) {
+        if (!this.has(node)) {
             this.checkedNodes.push(node);
         }
     }
@@ -22,40 +24,52 @@ class CheckedNode {
 }
 
 let trang = new Person(`Nguyen Minh Trang`, 23, `0978155603`);
-let duc = new Person(`Doan Nguyen Duc`, 27, `0938657673`);
-let trung = new Person(`Nguyen Duc Trung`, 25, `0978155603`);
-let mi = new Person(`Tran Ha Mi`, 24, `0874175703`);
-let tung = new Person(`Hoang Bach Tung`, 21, `0176165602`);
-let minh = new Person(`Do Tien Minh`, 29, `0978253624`);
-let khoan = new Person(`Phung Khac Khoan`, 33, `0874145203`);
+let duc = new Person(`Doan Nguyen Duc`, 27, `0878155603`);
+let trung = new Person(`Nguyen Duc Trung`, 25, `0778155603`);
+let mi = new Person(`Tran Ha Mi`, 24, `0678155603`);
+let tung = new Person(`Hoang Bach Tung`, 21, `0578155603`);
+let minh = new Person(`Do Tien Minh`, 29, `0478155603`);
+let khoan = new Person(`Phung Khac Khoan`, 33, `0378155603`);
 
 let soicialMap = new Map();
 soicialMap.set(trang, [duc, trung]);
 soicialMap.set(duc, [trang, mi, tung]);
 soicialMap.set(trung, [trang, tung]);
-soicialMap.set(tung, [duc, trung, khoan]);
+soicialMap.set(tung, [duc, trung, khoan, minh]);
 soicialMap.set(mi, [duc, khoan]);
 soicialMap.set(khoan, [mi, tung]);
 soicialMap.set(minh, [tung]);
 
-function breadthFirstSearch(person, soicialMap, callback) {
-    let stack = [];
-    let checkedPerson = new CheckedNode();
-    stack.push(person);
+// function breadthFirstSearch(person, soicialMap, callback) {
+//     let stack = [];
+//     let checkedPerson = new CheckedNode();
+//     stack.push(person);
+//     checkedPerson.add(person);
 
-    while (stack.length > 0) {
-        let currentPerson = stack.shift();
-        callback(currentPerson);
-        checkedPerson.add(currentPerson);
+//     while (stack.length > 0) {
+//         let currentPerson = stack.shift();
+//         callback(currentPerson);
 
-        for (let friend of soicialMap.get(currentPerson)) {
-            if (!checkedPerson.has(friend)) {
-                stack.push(friend);
-            }
+
+//         for (let friend of soicialMap.get(currentPerson)) {
+//             if (!checkedPerson.has(friend)) {
+//                 stack.push(friend);
+//                 checkedPerson.add(friend);
+//             }
+//         }
+//     }
+//     console.log(checkedPerson);
+// }
+let phoneNumber = prompt(`Please type your phone number`);
+
+function findPerson(phoneNumber) {
+    let person = null;
+    for (let [key, value] of soicialMap) {
+        if (key.phoneNumber == phoneNumber) {
+            person = key;
         }
     }
+    return person;
 }
 
-breadthFirstSearch(trang, soicialMap, (person) => {
-    console.log(`person:`, person);
-});
+console.log(soicialMap.get(findPerson(phoneNumber)));
